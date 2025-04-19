e95e3f08f96a97ef948be057eab5df005c5dd105
Batandwa <ghub@batandwa.me>
2025-04-19 04:17:59 +0200

    Add ESLint and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import { eslintConfigPrettier } from "eslint-config-prettier";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended", eslintConfigPrettier] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
