d0b75c4b663522a97cd2199bdfc4ffd46f4fb729
Batandwa <ghub@batandwa.me>
2025-04-19 04:35:36 +0200

    Update config import

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import { eslintConfigPrettier } from "eslint-config-prettier";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended", eslintConfigPrettier] },
