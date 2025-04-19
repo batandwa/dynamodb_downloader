45467c95f58c0303283dc501fa357d2a9836a82e
Batandwa <ghub@batandwa.me>
2025-04-19 04:46:49 +0200

    Format ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,13 @@ import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended", eslintConfigPrettier] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended", eslintConfigPrettier],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
 ]);
